4433e18380547eb6e7359a5697c9a585ebd6a6ac
Subhranil Maity <fightingdj1@gmail.com>
2021-06-28 10:22:09 -0700

    main

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let start = document.getElementsByClassName("start")[0];
+let store = document.getElementsByClassName("store")[0];
+
+
+start.addEventListener("click", ()=>{
+    window.open('http://stackoverflow.com', '_blank');
+})
+store.addEventListener("click", ()=>{
+    window.open('https://www.microsoft.com/en-in/store/apps/windows', '_blank');
+})
