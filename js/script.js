276eb90d49221ed971e81cff4e709a2df8ebb688
Subhranil Maity <fightingdj1@gmail.com>
2021-06-28 03:04:37 -0700

    added start menu and reflex

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 let start = document.getElementsByClassName("start")[0];
 let store = document.getElementsByClassName("store")[0];
+let sidebar = document.getElementById("hel")[0];
 
+document.getElementById("menu").style.bottom = "-655px";
 
-start.addEventListener("click", ()=>{
-    window.open('http://stackoverflow.com', '_blank');
+let startfunc = function (elss) {
+    if (document.getElementById("menu").style.bottom != "50px") {
+        if (elss == "yes") {
+            document.getElementById("menu").style.bottom = "50px";
+        }
+    } else {
+
+        document.getElementById("menu").style.bottom = "-655px";
+
+    }
+}
+
+start.addEventListener("click", () => {
+    startfunc(elss = "yes")
 })
-store.addEventListener("click", ()=>{
+store.addEventListener("click", () => {
     window.open('https://www.microsoft.com/en-in/store/apps/windows', '_blank');
 })
+
+function displayTime() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    startfunc()
+}
+
+const createClock = setInterval(displayTime, 15000);
